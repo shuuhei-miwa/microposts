1818e89c1f9a39890e2a94271d7fe7da01570360
shuuhei miwa <anonymity.syu@gmail.com>
2016-11-18 07:34:42 +0000

    Task1 validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :profile, length: { maximum: 400 }
+  validates :location, length: { maximum: 400 }
   has_secure_password
   validates_presence_of     :password, :on => :update
-  validates_presence_of     :password_confirmation, if: lambda { |m| m.password.present? }
 end
